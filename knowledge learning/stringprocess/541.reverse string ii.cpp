ccd49682662fdd21426feea3340094a5a3dfd49b
lichu <lj2231@nyu.edu>
2022-07-20 23:38:01 -0400

    string

--- knowledge learning/stringprocess/541.reverse string ii.cpp
+++ knowledge learning/stringprocess/541.reverse string ii.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution1 {
 public:
     string reverseStr(string s, int k) {
         int count=0;
@@ -13,6 +13,31 @@ public:
             }
             reverse(s.begin()+i,s.begin()+s.size());
         }
+        return s;
+    }
+};
+
+//my previous silly solution
+class Solution2 {
+public:
+    string reverseStr(string s, int k) {
+        int count=0;
+        int i=0;
+        if(s.size()==1){
+            return s;
+        }
+        while(i+k<s.size()){
+            //cout<<count<<endl;
+            //cout<<i<<endl;
+            if(count%2==0&&i+k<s.size()){
+                reverse(s.begin()+i,s.begin()+i+k);
+            }
+            count++;
+            i+=k;
+            //cout<<s<<endl;
+        }
+        if(count%2==0)
+            reverse(s.begin()+i,s.begin()+s.size());
         return s;
     }
 };
